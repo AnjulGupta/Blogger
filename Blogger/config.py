a9836649156a2951e2578ec0ea9ff67436471d7c
AnjulGupta <anjul.gupta@students.iiit.ac.in>
2019-10-16 19:20:15 +0530

    first commit

---
+++ Blogger/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import os
+WEBSITENAME = "Falcon"
+WEBSITEDESC = "\"With the new day comes new strength and new thoughts, go blogging\""
+DATABASE = os.path.join(os.getcwd(), "falcon11.db")
+DEBUG = True
+SECRET_KEY = os.urandom(20)
+USERNAME = 'admin'
+PASSWORD = 'girdhari@013' # If you forget to modify this. You probably deserve it.
